b347eff3b8457caa7ce76d725f1586f1e786e55f
pink222932 <pinkbearvivi@gmail.com>
2024-10-24 20:32:46 +0800

    Create ex2-2.cpp

---
+++ ex2-2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("1:Hello world\n1:\\I go to school by bus\n1:\I go to school by bus\n");
+    printf("2:apple\n");
+    printf("3:fever\n3:\\I play football after school\n3:\I play football after school\n");
+    printf("4:the ROC\n4:\\I'm from taiwan\n4:\I'm from taiwan\n");
+    printf("5:India\n");
+    printf("6:cold\n");
+    
+    return 0;
+}
