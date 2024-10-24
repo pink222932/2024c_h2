8ce9bbab0cce25674961a5c78e13e0b9eebff283
pink222932 <pinkbearvivi@gmail.com>
2024-10-24 20:54:58 +0800

    Create ex2-3.cpp

---
+++ ex2-3.cpp
@@ -0,0 +1,19 @@
+/*
+uioguoihoiugiougoi
+
+welcome to GDB Online 
+
+lhlubulbhluihbhuilb
+*/
+#include <stdio.h>
+int main()
+{
+
+     // this is comment by Elsa
+     printf("Hello World\n");// this is comment
+     //--------------------------
+     
+     
+     printf("Hello World output 2");
+     return 0;
+}
