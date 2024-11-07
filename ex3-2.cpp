7e2a2607fc6d9e0fe7e5e78b0cc3209dffaabd41
pink222932 <pinkbearvivi@gmail.com>
2024-11-07 20:03:03 +0800

    Create ex3-2.cpp

---
+++ ex3-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+  
+  
+  int i= 1;
+  float f;
+  char c;
+  int x ,y;
+  int sum;
+  
+  i = 10+ i;
+  f = 0.123456;
+  c = 'h';
+  x = 10;
+  y = 20;
+  sum = x+y;
+  printf("i= %d\n",i);
+  printf("f= %f\n",f);
+  printf("c= %c\n",c);
+  printf("x= %d\n",x);
+  printf("y= %d\n",y);
+  printf("sum = %d\n",sum);
+  return 0;
+}
