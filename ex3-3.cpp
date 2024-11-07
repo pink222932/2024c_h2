84f52d2786093f00bbb990d9db04ef89d55429c1
pink222932 <pinkbearvivi@gmail.com>
2024-11-07 20:21:40 +0800

    Create ex3-3.cpp

---
+++ ex3-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+  char a = 65, b = 66, c = 69;
+  //char a = 'a', b = 'b', c = 'c';
+    printf(" a = %c\n", a);
+    printf(" b = %c\n", b);
+    printf(" c = %c\n", c);
+    
+    return 0;
+}
