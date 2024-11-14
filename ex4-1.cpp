061f84a7b2ca72a5535009f6cc50efbdd6be2cbc
pink222932 <pinkbearvivi@gmail.com>
2024-11-14 19:20:31 +0800

    Create ex4-1.cpp

---
+++ ex4-1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+  int x,y;
+  
+  printf("please input int x=");
+  scanf("%d",&x);
+  printf("please input int y=");
+  scanf("%d",&y);
+  printf("x+y= %d\n",x+y); // x-y,x*y,x/y,x=y
+  printf("x+y= %d\n",x-y);
+  printf("x+y= %d\n",x*y);
+  printf("x+y= %d\n",x/y);
+
+    return 0;
+}
