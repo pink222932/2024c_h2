c680cd0629f13a7b85dedb9680bb988d3553c4d4
pink222932 <pinkbearvivi@gmail.com>
2024-11-14 20:31:49 +0800

    Create ex4-7.cpp

---
+++ ex4-7.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main()
+{
+  int x,y;
+  printf("please input int x=");
+  scanf("%d",&x);
+ 
+ switch (x){
+ case 0:
+ printf("sunday \n");
+ break;
+  case 1:
+ printf("monday \n");
+ break;
+  case 2:
+ printf("tuesday \n");
+ break;
+  case 3:
+ printf("wednesday \n");
+ break;
+  case 4:
+ printf("thursday \n");
+ break; 
+ case 5:
+ printf("friday \n");
+ break; 
+ case 6:
+ printf("saturday \n");
+ break;
+ 
+}
+    return 0;
+}
